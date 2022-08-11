8634ed41075da17adba4d829de6a47406e615f8d
GoBDaNWeb <bogdan.ognitsiy@yandex.ru>
2022-08-11 20:06:27 +0300

    add photo

--- src/constants/images.js
+++ src/constants/images.js
@@ -9,6 +9,7 @@ import redux from '../assets/redux.png';
 import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
+import profile from '../assets/profile.png';
 
 import circle from '../assets/circle.svg';
 
@@ -25,4 +26,5 @@ export default {
   typescript,
   vue,
   circle,
+  profile
 };
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -49,6 +49,7 @@ const Header = () => (
 			transition={{ duration: 0.5, delayChildren: 0.5 }}
 			className="header-img"
 		>
+			<img src={images.profile} alt="profile_bg" />
 		<motion.img
 			whileInView={{ scale: [0, 1] }}
 			transition={{ duration: 1, ease: 'easeInOut' }}
